b49943a5f05d870dc2b2939032dc876aa7b3d46a
mineralaz <jakimaviciuspovilas@gmail.com>
2022-11-15 14:02:06 +0200

    Task8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  sum(a, b) {
+    return a + b;
+  }
+  subtraction(a, b) {
+    return a - b;
+  }
+  multiplication(a, b) {
+    return a * b;
+  }
+  division(a, b) {
+    return a / b;
+  }
+}
+
+const calcClass = new Calculator();
+
+console.log("calcClass.sum(5, 5) ===", calcClass.sum(5, 5));
+console.log("calcClass.subtraction(5, 5) ===", calcClass.subtraction(5, 5));
+console.log(
+  "calcClass.multiplication(5, 5) ===",
+  calcClass.multiplication(5, 5)
+);
+console.log("calcClass.division(5, 5) ===", calcClass.division(5, 5));
