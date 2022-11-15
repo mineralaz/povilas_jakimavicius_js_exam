bf870124ef27758527421e081787dc9d4c01c9d4
mineralaz <jakimaviciuspovilas@gmail.com>
2022-11-15 08:59:29 +0200

    Task1 done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,45 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+const divEl = document.getElementById("output");
+const formEl = document.forms[0];
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const weightInpEl = document.getElementById("search");
+  const kg = weightInpEl.value.trim();
+
+  generateConvertedAnswer(kg);
+});
+
+/**
+ *
+ * Functions
+ *
+ */
+function toLb(kg) {
+  const result = kg * 2.2046;
+  return result;
+}
+function toG(kg) {
+  const result = kg / 0.001;
+  return result;
+}
+function toOz(kg) {
+  const result = kg * 35.274;
+  return result;
+}
+function crEl(tagName, destination, elClass, text) {
+  const el = document.createElement(tagName);
+  el.className = elClass;
+  el.textContent = text;
+  destination.append(el);
+  return el;
+}
+function createAnswerEl(params) {}
+function generateConvertedAnswer(kg) {
+  divEl.innerHTML = "";
+  crEl("h2", divEl, "toLb", `${toLb(kg)} lb`);
+  crEl("h2", divEl, "toG", `${toG(kg)} g`);
+  crEl("h2", divEl, "toOz", `${toOz(kg)} oz`);
+}
